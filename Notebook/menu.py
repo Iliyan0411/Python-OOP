80eaeac501d2ad4d171fdd8b989143003294bbfd
Iliyan0411 <ilian_dd00@abv.bg>
2021-02-05 18:49:44 +0200

    Notebook completed

--- Notebook/menu.py
+++ Notebook/menu.py
@@ -1,17 +1,75 @@
-from notebook import Notebook
+import sys
+from notebook import Note, Notebook
 
+class Menu:
+    def __init__(self):
+        self.notebook = Notebook()
+        self.choices = {
+                1: self.show_notes,
+                2: self.search_notes,
+                3: self.add_note,
+                4: self.modify_note,
+                5: self.quit
+                }
 
-def main():
-    n = Notebook()
+    def display_menu(self):
+        print("""
+        Notebook Menu
 
-    n.new_note('I love Spasi')
-    n.new_note('I ask who are you?')
-    n.new_note('Baby birthday')
-    n.new_note('Trip in Russia')
+        1. Show all notes
+        2. Search notes
+        3. Add new note
+        4. Modify note
+        5. Quit
+        """)
 
-    matches = n.search('I')
-    for m in matches: m.print_note()
+    def _user_input(self, minNum, maxNum, text):
+        choice = -1
 
+        while choice < minNum or choice > maxNum:
+            try:
+                choice = int(input(text))
+            except:
+                continue
+        
+        return choice
 
+    def run(self):
+        while True:
+            self.display_menu()
+            
+            choice = self._user_input(1, 5, 'Enter an option: ')
+            
+            action = self.choices.get(choice)
+            action()
 
-main()
+    def show_notes(self, notes=None):
+        if not notes:
+            notes = self.notebook.notes
+
+        for note in notes:
+            note.print_note()
+
+    def search_notes(self):
+        filter = input('Search for: ')
+        notes = self.notebook.search(filter)
+
+        self.show_notes(notes)
+
+    def add_note(self):
+        memo = input('Enter a memo: ')
+        tags = input('Enter tags: ')
+
+        self.notebook.new_note(memo, tags)
+
+    def modify_note(self):
+        id = int(input('Enter an id: '))
+        memo = input("Enter a memo: ")
+        tags = input("Enter tags: ")
+
+        self.notebook.modify_memo(id, memo)
+        self.notebook.modify_tags(id, tags)
+
+    def quit(self):
+        print("Thank you for using your notebook today.")
+        sys.exit(0)
--- Notebook/notebook.py
+++ Notebook/notebook.py
@@ -3,20 +3,20 @@ import datetime
 last_id = 0
 
 class Note:
-    def __init__(self, memo, tags=''):
+    def __init__(self, memo, tags):
         self.memo = memo
         self.tags = tags
         self.creation = datetime.date.today()
 
         global last_id
-        self.id = last_id
         last_id += 1
+        self.id = last_id
 
     def match(self, filter):
         return filter in self.memo or filter in self.tags
 
     def print_note(self):
-        print(f'--- {self.tags}')
+        print(f'{self.id}) {self.tags} \t [{self.creation}]')
         print(self.memo, '\n')
 
 
@@ -24,20 +24,21 @@ class Notebook:
     def __init__(self):
         self.notes = []
 
-    def new_note(self, memo, tags=''):
+    def new_note(self, memo, tags):
         self.notes.append(Note(memo, tags))
 
-    def modify_memo(self, note_id, memo):
+    def _find_note(self, note_id):
         for note in self.notes:
-            if note_id == note.id:
-                note.memo = memo
-                break
+            if note.id == note_id:
+                return note
+
+        return None
+
+    def modify_memo(self, note_id, memo):
+        self._find_note(note_id).memo = memo
 
     def modify_tags(self, note_id, tags):
-        for note in self.notes:
-            if note_id == note.id:
-                note.tags = tags
-                break
+        self._find_note(note_id).tags = tags
 
     def search(self, filter):
         result = []
